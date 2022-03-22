7e74fadba81179f57337e384f9a9139a6fdec4db
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-03-22 10:42:31 +0530

    Added solution - LeetHub

--- Wave Array - GFG/wave-array.cpp
+++ Wave Array - GFG/wave-array.cpp
@@ -11,9 +11,17 @@ class Solution{
     //Function to sort the array into a wave-like array.
     void convertToWave(vector<int>& arr, int n){
         
-        for (int i = 1; i < arr.size(); i += 2) {
-            swap(arr[i], arr[i - 1]);
-        }
+        // Traverse all even elements
+    for (int i = 0; i < n; i+=2)
+    {
+        // If current even element is smaller than previous
+        if (i>0 && arr[i-1] > arr[i] )
+            swap(arr[i], arr[i-1]);
+
+        // If current even element is smaller than next
+        if (i<n-1 && arr[i] < arr[i+1] )
+            swap(arr[i], arr[i + 1]);
+    }
     }
 };
 
