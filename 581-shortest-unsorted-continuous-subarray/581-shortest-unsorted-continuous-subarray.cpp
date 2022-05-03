db8d77b9dcdc80c0e1642fc6eeffeab386dd62e7
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-05-03 20:59:43 +0530

    Time: 38 ms (66.74%), Space: 28.2 MB (9.13%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> copy(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> smallestFromRight(n, nums[n - 1]);
+        vector<int> largestFromLeft(n, nums[0]);
         
-        sort(copy.begin(), copy.end());
+        for (int i = n - 2; i >= 0; i--)
+            smallestFromRight[i] = min(nums[i], smallestFromRight[i + 1]);
+        
+        for (int i = 1; i < n; i++)
+            largestFromLeft[i] = max(nums[i], largestFromLeft[i - 1]);
         
         int start = -1;
-        int end = -1;
-        for (int i = 0; i < nums.size(); i++) {
-            if (copy[i] != nums[i]) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > smallestFromRight[i]) {
                 start = i;
-                break;
-            }
-        }
-        
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] != copy[i]) {
-                end = i;
                 break;
             }
         }
@@ -24,6 +22,17 @@ public:
         if (start == -1)
             return 0;
         
+        int end = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < largestFromLeft[i]) {
+                end = i;
+                break;
+            }
+        }
+        
         return end - start + 1;
     }
 };
+
+// TC: O(n*log(n))
+// SC: O(n)
