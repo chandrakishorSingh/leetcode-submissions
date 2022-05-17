4207de0d39936976b661af4f0e9099dc811c3e5f
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-05-17 08:09:41 +0530

    Time: 796 ms (33.63%), Space: 166 MB (5.16%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -18,19 +18,22 @@ public:
         que.push({ original, cloned });
         
         TreeNode* result = nullptr;
-        while (!que.empty()) {
-            auto front = que.front();   que.pop();
-            
-            if (front.first->val == target->val) {
-                result = front.second;
-                break;
+        while (result == nullptr) {
+            int n = que.size();
+            for (int i = 0; i < n; i++) {
+                auto front = que.front();   que.pop();
+
+                if (front.first->val == target->val) {
+                    result = front.second;
+                    break;
+                }
+
+                if (front.first->left)
+                    que.push({ front.first->left, front.second->left });
+
+                if (front.first->right)
+                    que.push({ front.first->right, front.second->right });
             }
-            
-            if (front.first->left)
-                que.push({ front.first->left, front.second->left });
-            
-            if (front.first->right)
-                que.push({ front.first->right, front.second->right });
         }
         
         return result;
