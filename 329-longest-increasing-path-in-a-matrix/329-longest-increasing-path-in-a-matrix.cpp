ba72b693126d12eab79153d2c4b3515c0184b165
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-05-19 20:09:17 +0530

    Time: 206 ms (14.83%), Space: 30 MB (16.11%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -3,22 +3,19 @@ public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> visited(m, vector<int>(n));
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
         int result = 0;
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                result = max(result, dfs(i, j, matrix, visited, dp));
+                result = max(result, dfs(i, j, matrix, dp));
         
         return result;
     }
     
-    int dfs(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& visited, vector<vector<int>>& dp) {
+    int dfs(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
         if (dp[row][col] != -1)
             return dp[row][col];
-        
-        visited[row][col] = 1;
         
         vector<int> dx = {0, -1, 0, 1};
         vector<int> dy = {1, 0, -1, 0};
@@ -32,11 +29,9 @@ public:
             if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n)
                 continue;
             
-            if (visited[newRow][newCol] != 1 && matrix[newRow][newCol] > matrix[row][col])
-                pathLength = max(pathLength, 1 + dfs(newRow, newCol, matrix, visited, dp));
+            if (matrix[newRow][newCol] > matrix[row][col])
+                pathLength = max(pathLength, 1 + dfs(newRow, newCol, matrix, dp));
         }
-        
-        visited[row][col] = 0;
         
         dp[row][col] = pathLength;
         return dp[row][col];
