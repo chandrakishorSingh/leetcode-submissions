759d026bc67c1b4317bcec1b5e93b46d2fedde36
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-05-20 08:22:54 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (44.78%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> store(m, vector<int>(n, -1));
+
+        return explore(m - 1, n - 1, store);
+    }
+
+    int explore(int i, int j, vector<vector<int>>& store) {
+        if (store[i][j] != -1)
+            return store[i][j];
+
+        if (i == 0 || j == 0) {
+            return 1;
+        }
+
+        store[i][j] = explore(i - 1, j, store) + explore(i, j - 1, store);
+
+        return store[i][j];
+    }
+};
+
+// TC: O(m*n)
+// SC: O(m*n)
