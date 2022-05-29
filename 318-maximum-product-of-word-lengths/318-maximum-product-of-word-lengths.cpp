550664b34245041a7fc176ab9ffe36a053214323
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-05-29 12:30:05 +0530

    Time: 124 ms (37.92%), Space: 15.7 MB (79.69%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -3,26 +3,16 @@ public:
     int maxProduct(vector<string>& words) {
         int n = words.size();
 
-        vector<vector<int>> alphabets(n, vector<int>(26));
+        vector<int> mask(n);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < words[i].size(); j++)
-                alphabets[i][words[i][j] - 'a'] = 1;
+                mask[i] |= 1 << (words[i][j] - 'a');
 
         int result = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                bool hasCommonChars = false;
-                for (int k = 0; k < 26; k++) {
-                    if (alphabets[i][k] && alphabets[j][k]) {
-                        hasCommonChars = true;
-                        break;
-                    }
-                }
-
-                if (!hasCommonChars)
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (!(mask[i] & mask[j]))
                     result = max(result, (int)words[i].size() * (int)words[j].size());
-            }
-        }
 
         return result;
     }
