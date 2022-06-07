eee491fe838dd694b74057bdebc9590ac97b893b
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-07 10:52:24 +0530

    Time: 3 ms (72.68%), Space: 9.2 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = m - 1, j = 0; i >= 0; i--, j++)
-            nums1[m + n - 1 - j] = nums1[i];
+        for (int i = 0; i < n; i++)
+            nums1[i + m] = nums2[i];
         
-        int j = n;
-        int k = 0;
-        for (int i = 0; i < m + n; i++) {
-            if (k == n)
-                nums1[i] = nums1[j++];
-            else if (j == m + n)
-                nums1[i] = nums2[k++];
-            else
-                nums1[i] = (nums1[j] < nums2[k]) ? nums1[j++] : nums2[k++];
-        }
+        sort(nums1.begin(), nums1.end());
     }
 };
+
+// TC: O((m + n) * log(m + n))
+// SC: O(1)
