aa9b25c6fe00b162cb1ac79225e4b85bf41e868e
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-07 21:13:38 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (38.71%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        
+        unordered_map<int, int> freq;
+        for (auto num: nums1)
+            freq[num]++;
+        
+        for (auto num: nums2) {
+            auto it = freq.find(num);
+            
+            if (it == freq.end() || it->second == 0)
+                continue;
+            
+            it->second--;
+            result.push_back(num);
+        }
+        
+        return result;
+    }
+};
+
+// TC: O()
