550ed0de4227984778bea835a56bea4552bfb4b7
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-08 16:45:17 +0530

    Time: N/A, Memory: N/A',
      },
      questionId: '20',
      submissionId: '717258806',
      sessionId: '4602622',
      getLangDisplay: 'cpp',
      submissionCode: 'class Solution {\u000Apublic:\u000A    bool isValid(string s) {\u000A        stack\u003Cchar\u003E st\u003B\u000A        \u000A        for (int i \u003D 0\u003B i \u003C s.size()\u003B i++) {\u000A            if (s[i] \u003D\u003D \u0027(\u0027)\u000A                st.push(\u0027)\u0027)\u003B\u000A            else if (s[i] \u003D\u003D \u0027{\u0027)\u000A                st.push(\u0027}\u0027)\u003B\u000A            else if (s[i] \u003D\u003D \u0027[\u0027)\u000A                st.push(\u0027]\u0027)\u003B\u000A            else if (st.empty() || st.top() !\u003D s[i])\u000A                return false\u003B\u000A            else\u000A                st.pop()\u003B\u000A        }\u000A        \u000A        return st.empty()\u003B\u000A    }\u000A}\u003B',
      editCodeUrl: '/explore/item/721',
      checkUrl: '/submissions/detail/717258806/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022cpp\u0022, \u0022distribution\u0022: [[\u00220\u0022, 50.7318], [\u00221\u0022, 0.0072], [\u00222\u0022, 7.1013], [\u00223\u0022, 11.5717], [\u00224\u0022, 12.8691], [\u00225\u0022, 6.664], [\u00226\u0022, 5.1643]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022cpp\u0022, \u0022distribution\u0022: [[\u00225900\u0022, 0.0072], [\u00226000\u0022, 0.2204], [\u00226100\u0022, 3.5886], [\u00226200\u0022, 16.0818], [\u00226300\u0022, 28.6329], [\u00226400\u0022, 41.1008]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(')
+                st.push(')');
+            else if (s[i] == '{')
+                st.push('}');
+            else if (s[i] == '[')
+                st.push(']');
+            else if (st.empty() || st.top() != s[i])
+                return false;
+            else
+                st.pop();
+        }
+        
+        return st.empty();
+    }
+};
