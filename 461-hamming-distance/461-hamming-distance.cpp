9c49df6c550beefe67760c1069fa8b88a301b0f5
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-08 16:37:48 +0530

    Time: 0 ms (100.00%), Space: 6 MB (28.89%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result = 0;
+        
+        for (int i = 0; i < 31; i++)
+            if (((1 << i) & x) != ((1 << i) & y))
+                result++;
+        
+        return result;
+    }
+};
