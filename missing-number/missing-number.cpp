392f43f71fca0cbf9c0f99f72a044cea299acdf1
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-08 16:45:56 +0530

    Time: 35 ms, Memory: 17.8 MB - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == mid)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        
+        return low;
+    }
+};
+
+// TC: O(n)
+// SC: O(1)
