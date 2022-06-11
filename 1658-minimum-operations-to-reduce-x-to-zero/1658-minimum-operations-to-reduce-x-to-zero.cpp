4b76c2e610bcb8830e49b59c4192cdf693620e48
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-11 19:03:39 +0530

    Time: 409 ms (42.26%), Space: 98.6 MB (52.79%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,42 +2,41 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int n = nums.size();
-
-        vector<int> suffixSum(n + 1);
-        for (int i = n - 1; i >= 0; i--)
-            suffixSum[i] += suffixSum[i + 1] + nums[i];
         
+        int sum = 0;
+        for (auto num: nums)
+            sum += num;
+        
+        if (sum < x)
+            return -1;
+        
+        int i = n - 1;
+        int j = n;
         int result = INT_MAX;
-        int prefixSum = 0;
-        for (int i = 0; i <= n; i++) {
-            prefixSum += i > 0 ? nums[i - 1] : 0;
-            
-            int low = i;
-            int high = n;
-            int index = binarySearch(low, high, x - prefixSum, suffixSum);
-            
-            if (index != -1)
-                result = min(result, i + n - index);
+        while (i >= 0 || j >= 0) {
+            if (sum == x) {
+                result = min(result, i + 1 + n - j);
+
+                if (i == -1)
+                    break;
+                
+                sum -= nums[i--];
+            } else if (sum > x) {
+                if (i == -1)
+                    break; 
+                
+                sum -= nums[i--];
+            } else {
+                if (j == 0)
+                    break;
+                
+                sum += nums[--j];
+            }
         }
         
-        return result == INT_MAX ? -1: result;
-    }
-    
-    int binarySearch(int low, int high, int target, vector<int>& arr) {
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (arr[mid] == target)
-                return mid;
-            else if (arr[mid] < target)
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        
-        return -1;
+        return result == INT_MAX ? -1 : result;
     }
 };
 
-// TC: O(n) on average
+// TC: O(n * log(n))
 // SC: O(n)
