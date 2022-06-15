e27945e18d4e5bf7162a25a18644a9a02475633e
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-15 17:32:33 +0530

    Time: 158 ms (43.33%), Space: 16.5 MB (64.24%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        map<string, int> chainLength;
+        for (auto& word: words)
+            chainLength[word] = 1;
+        
+        sort(words.begin(), words.end(), [](string& s1, string& s2) {
+            if (s1.size() < s2.size())
+                return true;
+            return false;
+        });
+        
+        int result = 0;
+        for (auto& word: words) {
+            // cout << word << endl;
+            int maxChainLength = 0;
+            for (int i = 0; i < word.size(); i++) {
+                string prev = word.substr(0, i) + word.substr(i + 1);
+                
+                if (chainLength.count(prev)) {
+                    maxChainLength = max(maxChainLength, chainLength[prev]);
+                }
+            }
+            
+            chainLength[word] = maxChainLength + 1;
+            
+            result = max(result, maxChainLength + 1);
+        }
+        
+        return result;
+    }
+};
