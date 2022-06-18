3f990090621ffe72b4982d5c4b8847e91e17d503
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-18 12:14:21 +0530

    Time: 7 ms (73.97%), Space: 10.7 MB (16.38%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> st(nums1.begin(), nums1.end());
+        unordered_map<int, int> numExists;
+        for (auto num: nums1)
+            numExists[num] = 1;
         
         vector<int> result;
         for (auto num: nums2) {
-            if (st.count(num)) {
+            if (numExists[num] == 1) {
                 result.push_back(num);
-                st.erase(num);
+                numExists[num] = 0;
             }
         }
         
         return result;
     }
 };
+
+// TC: O(n*log(n) + m * log(n))
+// SC: O(n)
+// n, m are size of two arrays
