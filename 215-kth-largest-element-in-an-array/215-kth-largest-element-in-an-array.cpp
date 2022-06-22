12e2b302efb9a7dbf6d8f892dd6bbb7913b310c1
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-22 15:34:57 +0530

    Time: 10 ms (71.97%), Space: 9.9 MB (97.44%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,43 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        nth_element(nums.begin(), nums.end() - k, nums.end());
+        random_shuffle(nums.begin(), nums.end());
         
-        return nums[nums.size() - k];
+        int n = nums.size();
+        int targetIndex = n - k;
+        int low = 0;
+        int high = n - 1;
+        int partitionIndex;
+        while (true) {
+            partitionIndex = partition(nums, low, high);
+            if (partitionIndex == targetIndex)
+                break;
+            
+            if (partitionIndex < targetIndex)
+                low = partitionIndex + 1;
+            else
+                high = partitionIndex - 1;
+        }
+        
+        return nums[targetIndex];
+    }
+    
+    int partition(vector<int>& nums, int low, int high) {
+        int pivot = nums[low];
+        
+        int left = low + 1;
+        int right = high;
+        while (left <= right) {
+            if (nums[left] <= pivot) {
+                left++;
+            } else {
+                swap(nums[left], nums[right]);
+                right--;
+            }
+        }
+        
+        swap(nums[low], nums[left - 1]);
+        
+        return left - 1;
     }
 };
