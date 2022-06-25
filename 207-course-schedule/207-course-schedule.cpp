54a595b4d2f3d981eab675d342259252c1c4cad1
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-06-25 18:48:00 +0530

    Time: 29 ms (63.66%), Space: 14.4 MB (34.34%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses, vector<int>());
+        vector<int> indegree(numCourses);
+
+        for (auto prerequisite: prerequisites) {
+            int u = prerequisite[0];
+            int v = prerequisite[1];
+            
+            adjList[v].push_back(u);
+            indegree[u]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for (int i = 0; i < numCourses; i++)
+            pq.push({ indegree[i], i });
+        
+        bool result = true;
+        while (!pq.empty()) {
+            auto top = pq.top();    pq.pop();
+            
+            if (indegree[top.second] < 0)
+                continue;
+            
+            if (top.first != 0) {
+                result = false;
+                break;
+            }
+            
+            for (auto neighbour: adjList[top.second]) {
+                indegree[neighbour]--;
+                pq.push({ indegree[neighbour], neighbour });
+            }
+            
+            indegree[top.second] = -1;
+        }
+        
+        return result;
+    }
+};
+
+// TC: O(n(n + e))
+// SC: O(n + e)
