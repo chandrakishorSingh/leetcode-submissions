567302be048298e41ba14e2359cbd9a258b0e4e1
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-02 10:55:50 +0530

    Time: 119 ms (30.28%), Space: 32.3 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -7,19 +7,19 @@ public:
         int n = horizontalCuts.size();
         int m = verticalCuts.size();
         
-        int maxHorizontalCut = h - horizontalCuts[n - 1];
-        for (int i = 0, prev = 0; i < n; i++) {
-            maxHorizontalCut = max(maxHorizontalCut, horizontalCuts[i] - prev);
-            prev = horizontalCuts[i];
-        }
+        int maxHorizontalCut = max(horizontalCuts[0], h - horizontalCuts[n - 1]);
+        for (int i = 1; i < n; i++)
+            maxHorizontalCut = max(maxHorizontalCut, horizontalCuts[i] - horizontalCuts[i - 1]);
         
-        int maxVerticalCut = w - verticalCuts[m - 1];
-        for (int i = 0, prev = 0; i < m; i++) {
-            maxVerticalCut = max(maxVerticalCut, verticalCuts[i] - prev);
-            prev = verticalCuts[i];
-        }
+        int maxVerticalCut = max(verticalCuts[0], w - verticalCuts[m - 1]);
+        for (int i = 1; i < m; i++)
+            maxVerticalCut = max(maxVerticalCut, verticalCuts[i] - verticalCuts[i - 1]);
         
         int mod = 1e9 + 7;
         return ((int64_t)maxHorizontalCut * (int64_t)maxVerticalCut) % mod;
     }
 };
+
+// TC: O(k * log(k))
+// SC: O(1)
+// k = max(len(verticalCuts), len(horizontalCuts))
