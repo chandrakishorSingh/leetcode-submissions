f567f378865bd99831d5e66776ffd1e03f56fe74
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-15 17:09:31 +0530

    Time: 33 ms (43.83%), Space: 24 MB (25.82%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -4,34 +4,30 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
+        vector<vector<int>> visited(m, vector<int>(n));
         int result = 0;
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                result = max(result, dfs(i, j, grid));
-        
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (grid[i][j] == -1)
-                    grid[i][j] = 1;
-        
+                result = max(result, dfs(i, j, grid, visited));
+            
         return result;
     }
     
-    int dfs(int row, int col, vector<vector<int>>& grid) {
+    int dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& visited) {
         int m = grid.size();
         int n = grid[0].size();
         
-        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == -1 || grid[row][col] == 0)
+        if (row < 0 || row >= m || col < 0 || col >= n || visited[row][col] || grid[row][col] == 0)
             return 0;
         
-        grid[row][col] = -1;
+        visited[row][col] = 1;
         
         int result = 1;
         
-        result += dfs(row - 1, col, grid);
-        result += dfs(row, col + 1, grid);
-        result += dfs(row + 1, col, grid);
-        result += dfs(row, col - 1, grid);
+        result += dfs(row - 1, col, grid, visited);
+        result += dfs(row, col + 1, grid, visited);
+        result += dfs(row + 1, col, grid, visited);
+        result += dfs(row, col - 1, grid, visited);
         
         return result;
     }
