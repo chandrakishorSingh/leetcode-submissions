8cadad4ee70787b0b5ee229ff22f85ee7a99746c
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-16 14:18:31 +0530

    Time: 15 ms (57.00%), Space: 9.5 MB (35.21%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -6,19 +6,10 @@ public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         dp.resize(m + 1, vector<vector<int>>(n + 1, vector<int>(maxMove + 1, -1)));
         
-        // dp[startRow][startColumn][0] = 1;
-        
-//         int64_t result = 0;
-//         for (int i = 0; i < m; i++)
-//             for (int j = 0; j < n; j++)
-//                 if (i == 0 || j == 0 || i == m - 1 || j == n - 1)
-//                     result = ((result % mod) + (_findPaths(m, n, maxMove, i, j) % mod)) % mod;
-        
         return _findPaths(m, n, maxMove, startRow, startColumn);
     }
     
     int _findPaths(int m, int n, int remainingMoveCount, int currentRow, int currentColumn) {
-        if(remainingMoveCount < 0) return 0;
         if (currentRow < 0 || currentRow >= m || currentColumn < 0 || currentColumn >= n)
             return 1;
         
