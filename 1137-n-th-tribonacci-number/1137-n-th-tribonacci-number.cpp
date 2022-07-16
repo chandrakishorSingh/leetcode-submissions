b7f6acbf1f617aa7f2feeebf8db33db5176403a8
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-16 17:45:26 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.75%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -6,14 +6,22 @@ public:
         if (n == 1 || n == 2)
             return 1;
         
-        vector<int> values(n + 1);
-        values[0] = 0;
-        values[1] = 1;
-        values[2] = 1;
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int result = 2;
         
-        for (int i = 3; i <= n; i++)
-            values[i] = values[i - 1] + values[i - 2] + values[i - 3];
+        for (int i = 3; i <= n; i++) {
+            result = a + b + c;
+            
+            a = b;
+            b = c;
+            c = result;
+        }
         
-        return values[n];
+        return result;
     }
 };
+
+// TC: O(n)
+// SC: O(n)
