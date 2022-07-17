96d88bbb620655c0762a803c3186c0e92e37cded
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-17 14:33:24 +0530

    Time: 8 ms (50.00%), Space: 9.1 MB (100.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -5,18 +5,13 @@ public:
         
         for (auto num: nums) {
             buckets[num]++;
-            
-            if (buckets[num] == 2)
-                buckets[num] = 0;
         }
         
         vector<int> result(2);
         for (int i = 0; i <= 100; i++) {
-            if (buckets[i] == 1)
-                result[1]++;
+            result[0] += buckets[i] / 2;
+            result[1] += buckets[i] % 2;
         }
-        
-        result[0] = (nums.size() - result[1]) / 2;
         
         return result;
     }
