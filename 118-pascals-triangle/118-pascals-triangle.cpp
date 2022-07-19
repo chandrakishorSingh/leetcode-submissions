ed8c33405fbbf8c05e641a43466e340837013a04
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-19 11:14:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result = {{1}, {1, 1}};
+        vector<vector<int>> result = {{1}};
 
-        if (numRows <= 2)
-            return vector<vector<int>>(result.begin(), result.begin() + numRows);
-        
-        for (int i = 3; i <= numRows; i++) {
+        for (int i = 2; i <= numRows; i++) {
             vector<int> currentRow({1});
             for (int j = 1; j < result.back().size(); j++)
                 currentRow.push_back(result[i - 2][j] + result[i - 2][j - 1]);
@@ -19,3 +16,6 @@ public:
         return result;
     }
 };
+
+// TC: O(n^2)
+// SC: O(1), except the output
