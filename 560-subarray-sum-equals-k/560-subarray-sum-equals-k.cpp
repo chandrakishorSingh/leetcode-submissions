b52a2551e8bf7b89a44f46462e6bf1f2973a2762
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-20 11:12:59 +0530

    Time: 164 ms (33.37%), Space: 41.5 MB (72.66%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        map<int, int> cumulativeSumFreq;
+        unordered_map<int, int> cumulativeSumFreq;
         cumulativeSumFreq[0] = 1;
         
         int cumulativeSum = 0;
