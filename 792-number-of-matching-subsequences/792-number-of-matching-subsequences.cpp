6d10938370d0f2a92ba70fb3c7451eff1fd16cbd
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-07-22 12:11:52 +0530

    Time: 1077 ms (12.23%), Space: 235.6 MB (26.13%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int n = s.size();
+        vector<vector<int>> charToSubarray(26, vector<int>(n, -1));
+        
+        charToSubarray[s.back() - 'a'][n - 1] = n - 1;
+        
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < 26; j++) {
+                charToSubarray[j][i] = charToSubarray[j][i + 1];
+            }
+            
+            charToSubarray[s[i] - 'a'][i] = i;
+        }
+        
+        // for (auto& row: charToSubarray) {
+        //     for (auto num: row) {
+        //         cout << num << " ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        int result = 0;
+        for (auto& word: words)
+            if (isSubSequenceOf(word, charToSubarray))
+                result++;
+        
+        return result;
+    }
+    
+    bool isSubSequenceOf(string subSequence, vector<vector<int>>& charToSubarray) {
+        bool result = true;
+        int windowStartingIndex = 0;
+        int n = charToSubarray[0].size();
+        int m = subSequence.size();
+        
+        // cout << subSequence << endl;
+        for (int i = 0; i < m; i++) {
+            // cout << windowStartingIndex << endl;
+            
+            if (windowStartingIndex >= n) {
+                result = false;
+                break;
+            }
+                
+            windowStartingIndex = charToSubarray[subSequence[i] - 'a'][windowStartingIndex];
+            
+            if (windowStartingIndex == -1) {
+                result = false;
+                break;
+            }
+            
+            windowStartingIndex++;
+        }
+        
+        return result;
+    }
+};
