4a1cbd1a08c6db9419e2225a8869a7ebc2cce6f0
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-04 16:44:50 +0530

    Time: 4 ms (23.97%), Space: 5.8 MB (90.91%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,46 +1,11 @@
 class Solution {
 public:
     int mirrorReflection(int p, int q) {
-        int directionX = 1;
-        int directionY = 1;
-        int y = q;
+        int lcm = (p * q) / __gcd(p, q);
         
-        int result = -1;
-        while (true) {
-            if (directionX == 1 && y == 0) {
-                result = 0;
-                break;
-            }
-
-            if (directionX == 1 && y == p) {
-                result = 1;
-                break;
-            }
-            
-            if (directionX == -1 && y == p) {
-                result = 2;
-                break;
-            }
-            
-            if (directionY == 1) {
-                y += q;
-                
-                if (y > p) {
-                    y = 2 * p - y;
-                    directionY = -directionY;
-                }
-            } else {
-                y -= q;
-                
-                if (y < 0) {
-                    y = -y;
-                    directionY = -directionY;
-                }
-            }
-            
-            directionX = -directionX;
-        }
+        if ((lcm / q) % 2 == 0)
+            return 2;
         
-        return result;
+        return (lcm / p) % 2 == 0 ? 0 : 1;
     }
 };
