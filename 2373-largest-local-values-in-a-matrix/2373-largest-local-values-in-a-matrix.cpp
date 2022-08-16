5ba436b7f4389d4a8fefeb87bd8c49374c365aad
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-16 11:04:07 +0530

    Time: 45 ms (28.57%), Space: 14.3 MB (14.29%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -1,15 +1,31 @@
 class Solution {
+vector<int> slideMax(vector<vector<int>>&vec, int k,int col){
+        priority_queue<pair<int,int>>pq;
+        vector<int>res;
+        pq.push({vec[0][col],0});
+        for(int i = 1;i<vec.size();i++){
+            while(pq.top().second+k<=i)
+                pq.pop();
+            pq.push({vec[i][col],i});
+            if(i>=k-1)
+                res.push_back(pq.top().first);
+        }
+        return res;
+    }
+    
 public:
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<vector<int>> result(n - 2, vector<int>(n - 2, INT_MIN));
+        vector<vector<int>>res(n);
+        for(int j = 0;j<n;j++)
+            res[j] = slideMax(grid,3,j);
         
-        for (int i = 1; i < n - 1; i++)
-            for (int j = 1; j < n - 1; j++)
-                for (int k = i - 1, rowCount = 0; rowCount < 3; rowCount++, k++)
-                    for (int l = j - 1, colCount = 0; colCount < 3; colCount++, l++)
-                        result[i - 1][j - 1] = max(result[i - 1][j - 1], grid[k][l]);
-        
-        return result;
+        vector<vector<int>>ans(n-2);
+        for(int j = 0;j<n-2;j++)
+            ans[j] = slideMax(res,3,j);
+        return ans;
     }
 };
+
+// TC: O(m * n)
+// SC: O(1), except the size for output
