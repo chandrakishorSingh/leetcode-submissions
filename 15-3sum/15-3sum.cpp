75570d859ac23e5f999baa4b4a7b72eaa302ff05
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-17 20:19:51 +0530

    Time: 136 ms (59.25%), Space: 19.9 MB (89.42%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -4,9 +4,12 @@ public:
         sort(nums.begin(), nums.end());
 
         vector<vector<int>> result;
-        for (int i = 0; i < nums.size() - 2 && nums[i] <= 0; i++) {
+        for (int i = 0; i < nums.size() - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
+            
+            if (nums[i] > 0)
+                break;
             
             twoSum(-nums[i], i + 1, nums, result);
         }
