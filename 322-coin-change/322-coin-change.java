d9f5a2f1c13049b894b198dc1e3e98761f3aa1b0
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-22 18:18:47 +0530

    Time: 120 ms (10.76%), Space: 72.8 MB (7.65%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,15 +1,16 @@
 class Solution {
+
     public int coinChange(int[] coins, int amount) {
         
-        int[][] dp=new int[coins.length][amount+1];
-        for(int[] x:dp)Arrays.fill(x,-1);
+         Integer[][] dp=new Integer[coins.length][amount+1];
+        //for(int[] x:dp)Arrays.fill(x,-1);
         int res=f(coins.length-1,coins,amount,dp);
         return res>=(int)1e9?-1:res;
     }
-    public int f(int i,int[] coins, int amt,int[][] dp) {
+    public int f(int i,int[] coins, int amt,Integer[][] dp) {
         if(amt==0)return 0;
         if(i<0) return (int)1e9;
-        if(dp[i][amt]!=-1)return dp[i][amt];
+        if(dp[i][amt]!=null)return dp[i][amt];
         int pick=(int)1e9;
         if(amt>=coins[i]) pick=1+f(i,coins,amt-coins[i],dp);
         
