a46fdfdf159efbbdfe1b5d7dff523a203cfe3b6c
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-22 20:26:22 +0530

    Time: 162 ms (42.29%), Space: 50.2 MB (8.38%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
+    int infinity = 1e4 + 1;
+    
     int coinChange(vector<int>& coins, int amount) {
-        int infinity = 1e4 + 1;
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
         
-        vector<int> dp(amount + 1, infinity);
-        dp[0] = 0;
+        auto result = _coinChange(n, coins, amount, dp);
         
-        for (int i = 0; i < coins.size(); i++)
-            for (int j = 1; j <= amount; j++)
-                if (j >= coins[i] && dp[j - coins[i]] != infinity)
-                    dp[j] = min(dp[j], 1 + dp[j - coins[i]]);
+        return result == infinity ? -1 : result;
+    }
+    
+    int _coinChange(int n, vector<int>& coins, int amount, vector<vector<int>>& dp) {
+        if (amount == 0)
+            return 0;
         
-        return dp[amount] == infinity ? -1: dp[amount];
+        if (n == 0)
+            return infinity;
+        
+        if (dp[n][amount] != -1)
+            return dp[n][amount];
+        
+        int option1 = infinity;
+        if (coins[n - 1] <= amount)
+            option1 = 1 + _coinChange(n, coins, amount - coins[n - 1], dp);
+        
+        int option2 = _coinChange(n - 1, coins, amount, dp);
+        
+        dp[n][amount] = min(option1, option2);
+        
+        return dp[n][amount];
     }
 };
+
+// TC: O(2^(n + (amount / (min(coins[i])))))
+// SC: O(n + (amount / min(coins[i])))
