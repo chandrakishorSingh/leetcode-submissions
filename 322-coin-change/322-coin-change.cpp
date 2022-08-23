36cd7a7318a7147c99d7fa5c2c6e4389ef4248a3
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-23 11:00:34 +0530

    Time: 151 ms (48.80%), Space: 14.6 MB (54.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
         int infinity = 1e4 + 1;
         
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
+        vector<int> dp(amount + 1, infinity);
+        dp[0] = 0;
         
-        for (int i = 1; i <= amount; i++)
-            dp[0][i] = infinity;
-        
-        for (int i = 1; i <= n; i++)
+        for (int i = 0; i < coins.size(); i++)
             for (int j = 1; j <= amount; j++)
-                dp[i][j] = min(j >= coins[i - 1] ? 1 + dp[i][j - coins[i - 1]] : dp[i - 1][j], dp[i - 1][j]);
+                dp[j] = min(dp[j], j >= coins[i] ? (1 + dp[j - coins[i]]) : dp[j]);
         
-        return dp[n][amount] == infinity ? -1 : dp[n][amount];
+        return dp[amount] == infinity ? -1 : dp[amount];
     }
 };
 
 // TC: O(n * amount)
-// SC: O(n * amount)
+// SC: O(amount)
