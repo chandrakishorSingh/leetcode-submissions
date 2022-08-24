4fd0684c9c09010f3e01c62057972f314a4ecafc
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-24 23:56:26 +0530

    Time: 30 ms (35.35%), Space: 9.1 MB (39.74%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,31 +4,21 @@ public:
         int n1 = word1.size();
         int n2 = word2.size();
         
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
         
-        return _minDistance(n1, n2, word1, word2, dp);
-    }
-    
-    int _minDistance(int n1, int n2, string& word1, string& word2, vector<vector<int>>& dp) {
-        if (n1 == 0 || n2 == 0)
-            return n1 + n2;
+        for (int i = 0; i <= n1; i++)
+            dp[i][0] = i;
         
-        if (dp[n1][n2] != -1)
-            return dp[n1][n2];
+        for (int i = 0; i <= n2; i++)
+            dp[0][i] = i;
         
-        int result = -1;
-        if (word1[n1 - 1] == word2[n2 - 1]) {
-            result = _minDistance(n1 - 1, n2 - 1, word1, word2, dp);
-        } else {
-            result = min({
-                1 + _minDistance(n1, n2 - 1, word1, word2, dp),
-                1 + _minDistance(n1 - 1, n2, word1, word2, dp),
-                1 + _minDistance(n1 - 1, n2 - 1, word1, word2, dp),
-            });
-        }
-        
-        dp[n1][n2] = result;
+        for (int i = 1; i <= n1; i++)
+            for (int j = 1; j <= n2; j++)
+                dp[i][j] = word1[i - 1] == word2[j - 1] ? dp[i - 1][j - 1] : 1 + min({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] });
         
         return dp[n1][n2];
     }
 };
+
+// TC: O(3^(n1 + n2))
+// SC: O(n1 + n2)
