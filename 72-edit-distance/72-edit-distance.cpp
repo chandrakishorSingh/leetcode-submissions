9c96d39793a60136b00d21743b455ba5532551e7
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-25 09:20:23 +0530

    Time: 20 ms (56.56%), Space: 6.4 MB (94.63%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -4,21 +4,25 @@ public:
         int n1 = word1.size();
         int n2 = word2.size();
         
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
-        
-        for (int i = 0; i <= n1; i++)
-            dp[i][0] = i;
-        
+        vector<int> dp(n2 + 1);
         for (int i = 0; i <= n2; i++)
-            dp[0][i] = i;
+            dp[i] = i;
         
-        for (int i = 1; i <= n1; i++)
-            for (int j = 1; j <= n2; j++)
-                dp[i][j] = word1[i - 1] == word2[j - 1] ? dp[i - 1][j - 1] : 1 + min({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] });
+        for (int i = 1; i <= n1; i++) {
+            int prev = dp[0];
+            dp[0] = i;
+            
+            for (int j = 1; j <= n2; j++) {
+                // cout << aux << endl;
+                int temp = dp[j];
+                dp[j] = word1[i - 1] == word2[j - 1] ? prev : 1 + min({ dp[j], dp[j - 1], prev });
+                prev = temp;
+            }
+        }
         
-        return dp[n1][n2];
+        return dp[n2];
     }
 };
 
-// TC: O(3^(n1 + n2))
-// SC: O(n1 + n2)
+// TC: O(n1 * n2)
+// SC: O(n1 * n2)
