6003cb25cb1ee8158aeaa101d65df9d310d1c47d
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-26 11:05:23 +0530

    Time: 230 ms (33.44%), Space: 53.5 MB (53.59%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,10 @@
+var groupAnagrams = function(strs) {
+    let res = {};
+    for (let str of strs) {
+        let count = new Array(26).fill(0);
+        for (let char of str) count[char.charCodeAt()-97]++;
+        let key = count.join("#");
+        res[key] ? res[key].push(str) : res[key] = [str];
+    }
+    return Object.values(res);
+};
