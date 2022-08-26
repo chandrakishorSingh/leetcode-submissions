b96f447b8d4ddf6b25a0e50e4e3020ec95cd5255
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-08-26 12:58:03 +0530

    Time: 131 ms (64.44%), Space: 41.3 MB (95.15%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int result = 0;
+        unordered_map<int, int> cumulativeSumFreq;
+        cumulativeSumFreq[0] = 1;
 
-        unordered_map<int, int> cumulativeSumFreq({{0, 1}});
         int cumulativeSum = 0;
-        for (int i = 0; i < n; i++) {
-            cumulativeSum += nums[i];
-            
-            if (cumulativeSumFreq.count(cumulativeSum - k)) {
-                result += cumulativeSumFreq[cumulativeSum - k];
-            }
-            
+        int result = 0;
+        for (auto num: nums) {
+            cumulativeSum += num;
+
+            auto it = cumulativeSumFreq.find(cumulativeSum - k);
+            if (it != cumulativeSumFreq.end())
+                result += it->second;
+
             cumulativeSumFreq[cumulativeSum]++;
         }
-        
+
         return result;
     }
 };
 
-// TC: O(n^2)
-// SC: O(1)
+// TC: Θ(n)
+// SC: O(n)
