5db66dad3c1bb2b6f8afbedeedd9b489846bf03e
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-09-05 10:33:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (100.00%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,51 +1,9 @@
 class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
-        bool result = true;
-        
-        for (int base = 2; base <= n - 2; base++) {
-            auto numString = toBase(base, n);
-            
-            if (!isPalindrome(numString)) {
-                result = false;
-                break;
-            }
-        }
-        
-        return result;
-    }
-    
-    string toBase(int base, int number) {
-        stack<int> st;
-        
-        while (number != 0) {
-            st.push(number % base);
-            number /= base;
-        }
-        
-        string result = "";
-        while (!st.empty()) {
-            char digit = st.top() + '0';
-            result.push_back(digit);
-            
-            st.pop();
-        }
-        
-        return result;
-    }
-    
-    bool isPalindrome(string& str) {
-        int left = 0;
-        int right = str.size() - 1;
-        
-        while (left < right) {
-            if (str[left] != str[right])
-                return false;
-            
-            left++;
-            right--;
-        }
-        
-        return true;
+        return false;
     }
 };
+
+// TC: O(1)
+// SC: O(1)
