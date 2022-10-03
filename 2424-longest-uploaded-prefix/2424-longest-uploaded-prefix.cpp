ca7bc9fed29f0374c00ecab2f45a00beb02a8a45
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-10-03 17:19:06 +0530

    Time: 999 ms (20.00%), Space: 162.9 MB (20.00%) - LeetHub

--- 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
@@ -1,71 +1,42 @@
-class UF {
- public:
-  vector<int> parent;
-  vector<int> size;
-  int components;
-
-  UF(int n) {
-    parent.resize(n + 1);
-    size.resize(n + 1, 1);
-    components = n;
-
-    for (int i = 1; i <= n; i++) {
-      parent[i] = i;
-    }
-  }
-
-  void unite(int a, int b) {
-    a = find(a);
-    b = find(b);
-
-    if (a == b) return;
-
-    if (size[a] < size[b]) swap(a, b);
-
-    parent[b] = a;
-    size[a] += size[b];
-    components--;
-  }
-
-  int find(int a) {
-    if (a == parent[a]) return a;
-
-    int root = find(parent[a]);
-    parent[a] = root;
-
-    return root;
-  }
-};
-
 class LUPrefix {
 public:
     int size;
-    UF* uf;
-    vector<int> videoIndex;
+    vector<int> videos;
+    vector<int> leftBlockIndex;
+    vector<int> rightBlockIndex;
     
     LUPrefix(int n) {
         size = n;
-        
-        uf = new UF(n + 1);
-        
-        videoIndex.resize(n + 2);
-        videoIndex[1] = 1;
+        videos.resize(size + 1, -1);
+        leftBlockIndex.resize(size + 1);
+        rightBlockIndex.resize(size + 1);
     }
     
     void upload(int video) {
-        videoIndex[video + 1] = 1;
+        int leftEnd = -1;
+        int rightEnd = -1;
         
-        if (videoIndex[video] == 1) {
-            uf->unite(video + 1, video);
+        if (video != 1 && video != size && videos[video - 1] != -1 && videos[video + 1] != -1) {
+            leftEnd = leftBlockIndex[video - 1];
+            rightEnd = rightBlockIndex[video + 1];
+        } else if (video != size && videos[video + 1] != -1) {
+            leftEnd = video;
+            rightEnd = rightBlockIndex[video + 1];
+        } else if (video != 1 && videos[video -  1] != -1) {
+            leftEnd = leftBlockIndex[video - 1];
+            rightEnd = video;
+        } else {
+            leftEnd = video;
+            rightEnd = video;
         }
         
-        if (video != size && videoIndex[video + 2]) {
-            uf->unite(video + 1, video + 2);
-        }
+        rightBlockIndex[leftEnd] = rightEnd;
+        leftBlockIndex[rightEnd] = leftEnd;
+        videos[video] = 1;
     }
     
     int longest() {
-        return uf->size[uf->find(1)] - 1;
+        return rightBlockIndex[1];
     }
 };
 
@@ -76,5 +47,5 @@ public:
  * int param_2 = obj->longest();
  */
 
-// TC: O(n * noOfQueries)
+// TC: O(total number of calls to upload() and longest())
 // SC: O(n)
