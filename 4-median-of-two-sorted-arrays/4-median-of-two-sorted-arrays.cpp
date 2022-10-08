4c7b39f689819bb3dc31d79257b53eab70f1be1e
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-10-08 10:46:59 +0530

    Time: 55 ms (74.19%), Space: 89.3 MB (65.24%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> arr;
-        for (auto num: nums1)
-            arr.push_back(num);
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         
-        for (auto num: nums2)
-            arr.push_back(num);
+        int middleIndex = (n1 + n2) / 2 + 1;
         
-        sort(arr.begin(), arr.end());
-        
-        int middleIndex = arr.size() / 2;
-        return arr.size() % 2 == 0 ? (arr[middleIndex] + arr[middleIndex - 1]) / 2.0 : arr[middleIndex];
+        int i = 0;
+        int j = 0;
+        int previousElement = 0;
+        int middleElement = 0;
+        while (middleIndex--) {
+            previousElement = middleElement;
+            
+            if (i == n1) {
+                middleElement = nums2[j++];
+            } else if (j == n2) {
+                middleElement = nums1[i++];
+            } else if (nums1[i] <= nums2[j]) {
+                middleElement = nums1[i++];
+            } else {
+                middleElement = nums2[j++];
+            }
+            
+        }
+
+        return (n1 + n2) % 2 == 0 ? ((middleElement + previousElement) / 2.0): middleElement;
     }
 };
-// TC: O((m + n) * log(m + n))
-// SC: O(m + n)
