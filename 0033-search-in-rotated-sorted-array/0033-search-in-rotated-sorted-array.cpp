6e14c385eb1b6cff72c6834331422773ced17563
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-10-12 08:40:22 +0530

    Time: 7 ms (61.08%), Space: 11.2 MB (29.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int search_element) {
-int low = 0, high = arr.size();
-
-    while (low < high) {
-        int mid = low + (high - low) / 2;
-
-        int curr;
-
-        // Check if search_element
-        // falls in first half or
-        // second half
-        if ((arr[mid] < arr[0])
-            == (search_element < arr[0])) {
-            curr = arr[mid];
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        int result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            int current;
+            if ((nums[0] <= nums[mid]) == (nums[0] <= target)) {
+                current = nums[mid];
+            } else if (target < nums[0]) {
+                current = INT_MIN;
+            } else {
+                current = INT_MAX;
+            }
+            
+            if (current == target) {
+                result = mid;
+                break;
+            } else if (current < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-
-        // If search_element falls in
-        // second half take min number
-        else if (search_element < arr[0]) {
-            curr = INT_MIN;
-        }
-
-        // If search_element falls in
-        // first half take max number
-        else {
-            curr = INT_MAX;
-        }
-
-        // Perform binary search
-        // to find the key
-        if (curr < search_element) {
-            low = mid + 1;
-        }
-        else if (curr > search_element) {
-            high = mid;
-        }
-        else {
-            return mid;
-        }
-    }
-
-    return -1;
+        
+        return result;
     }
 };
