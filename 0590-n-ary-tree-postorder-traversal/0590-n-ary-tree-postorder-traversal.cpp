625ac9d172b5d2aaba3db21f534167c7092cb46a
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-11-07 14:58:48 +0530

    Time: 43 ms (67.18%), Space: 11.3 MB (50.64%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -22,19 +22,26 @@ class Solution {
 public:
     vector<int> postorder(Node* root) {
         vector<int> result;
+        stack<Node*> st({root});
         
-        postorder(root, result);
+        while (!st.empty()) {
+            auto node = st.top();
+            st.pop();
+            
+            if (node == nullptr)
+                continue;
+            
+            result.push_back(node->val);
+            
+            for (auto& neighbour: node->children)
+                st.push(neighbour);
+        }
+        
+        reverse(result.begin(), result.end());
         
         return result;
     }
-    
-    void postorder(Node* root, vector<int>& result) {
-        if (root == nullptr)
-            return;
-        
-        for (auto& neighbour: root->children)
-            postorder(neighbour, result);
-        
-        result.push_back(root->val);
-    }
 };
+
+// TC: O(n)
+// SC: O(n)
