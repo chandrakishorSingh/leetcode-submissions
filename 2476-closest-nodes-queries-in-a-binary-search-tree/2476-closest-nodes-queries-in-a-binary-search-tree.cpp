106810055fe028e4307ccf12b91c800d39576283
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2022-11-23 14:34:04 +0530

    Time: 1134 ms (15.80%), Space: 157 MB (7.59%) - LeetHub

---
+++ 2476-closest-nodes-queries-in-a-binary-search-tree/2476-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ceil(vector<int>& values, int val) {
+        int low = 0;
+        int high = values.size() - 1;
+        
+        int result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (values[mid] == val)
+                return val;
+            
+            if (values[mid] > val) {
+                result = values[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return result;
+    }
+    
+    int floor(vector<int>& values, int val) {
+        int low = 0;
+        int high = values.size() - 1;
+        
+        int result = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (values[mid] == val)
+                return val;
+            
+            if (values[mid] < val) {
+                result = values[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+    
+    void inorder(TreeNode* root, vector<int>& nums) {
+        if (root == nullptr)
+            return;
+        
+        inorder(root->left, nums);
+        nums.push_back(root->val);        
+        inorder(root->right, nums);
+    }
+    
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
+        vector<int> values;
+        inorder(root, values);
+        
+        vector<vector<int>> result;
+        
+        for (auto& query: queries) {
+            result.push_back({ floor(values, query), ceil(values, query) });
+        }
+        
+        return result;
+    }
+};
