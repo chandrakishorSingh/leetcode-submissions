3ac3f0f8d14ba878e6dc9ae290e562f822dcee9b
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-02 16:14:39 +0530

    Time: 138 ms (36.36%), Space: 18.9 MB (45.45%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     int distinctPrimeFactors(vector<int>& nums) {
-        int maxElement = *max_element(nums.begin(), nums.end());
-        vector<int> primes = getPrimeNumbers(maxElement);
+        set<int> primes;
         
-        int result = 0;
-        for (int i = 0; i < primes.size(); i++) {
-            for (auto num: nums) {
-                if (num % primes[i] == 0) {
-                    result++;
-                    break;
+        for (auto num: nums) {
+            int divisor = 2;
+            while (num != 1) {
+                bool flag = false;
+                while (num % divisor == 0) {
+                    num /= divisor;
+                    flag = true;
                 }
+                
+                if (flag) {
+                    primes.insert(divisor);
+                }
+                
+                divisor++;
             }
         }
         
-        return result;
-    }
-    
-    vector<int> getPrimeNumbers(int upperLimit) {
-        vector<int> nums(upperLimit + 1);
-        
-        for (int i = 2; i <= upperLimit; i++) {
-            if (nums[i]) {
-                continue;
-            }
-            
-            for (int j = 2 * i; j <= upperLimit; j += i) {
-                nums[j] = 1;
-            }
-        }
-        
-        vector<int> result;
-        for (int i = 2; i <= upperLimit; i++) {
-            if (nums[i] != 1) {
-                result.push_back(i);
-            }
-        }
-        
-        return result;
+        return primes.size();
     }
 };
 
