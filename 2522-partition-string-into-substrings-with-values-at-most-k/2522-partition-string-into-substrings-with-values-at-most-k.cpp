1e147e8a4920c6d914147e2ea0dac97ad7518293
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-02 17:47:05 +0530

    Time: 13 ms (100.00%), Space: 9.1 MB (100.00%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int minimumPartition(string s, int k) {
-        if (k <= 9) {
-            int maxDigit = *max_element(s.begin(), s.end()) - '0';
-            
-            if (maxDigit > k)
-                return -1;
-        }
-        
         int result = 1;
         int64_t currentValue = 0;
         for (int i = 0; i < s.size(); i++) {
@@ -16,6 +9,11 @@ public:
             if (currentValue > k) {
                 currentValue = s[i] - '0';
                 result++;
+            }
+            
+            if (currentValue > k) {
+                result = -1;
+                break;
             }
         }
         
