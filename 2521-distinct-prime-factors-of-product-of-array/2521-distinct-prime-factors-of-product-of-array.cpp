caa25509e4ad5a2cf2e339ef9cae8d0a6865be79
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-02 11:55:03 +0530

    Time: 188 ms (36.36%), Space: 19.6 MB (18.18%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        vector<int> primes = getPrimeNumbers(1000);
+        
+        set<int> seen;
+        for (auto num: nums) {
+            for (int i = 0; i < primes.size() and num != 0; i++) {
+                bool flag = false;
+                
+                while (num % primes[i] == 0) {
+                    num /= primes[i];
+                    flag = true;
+                }
+                
+                if (flag) {
+                    seen.insert(primes[i]);
+                }
+            }
+        }
+        
+        return seen.size();
+    }
+    
+    vector<int> getPrimeNumbers(int upperLimit) {
+        vector<int> nums(upperLimit + 1);
+        
+        for (int i = 2; i <= upperLimit; i++) {
+            for (int j = 2 * i; j <= upperLimit; j += i) {
+                nums[j] = 1;
+            }
+        }
+        
+        vector<int> result;
+        for (int i = 2; i <= upperLimit; i++) {
+            if (nums[i] != 1) {
+                result.push_back(i);
+            }
+        }
+        
+        return result;
+    }
+};
