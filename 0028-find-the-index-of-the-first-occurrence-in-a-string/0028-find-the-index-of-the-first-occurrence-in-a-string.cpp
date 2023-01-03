fc1bf7a8d55c79f5e84fb5922f5c736a1377d2ee
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-03 18:33:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (30.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,8 +1,44 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        auto index = haystack.find(needle);
+        int n = needle.size();
+        vector<int> lps(n);
         
-        return index == string::npos ? -1 : index;
+        for (int i = 1; i < n; i++) {
+            int j = lps[i - 1];
+            
+            while (j > 0 && needle[j] != needle[i]) {
+                j = lps[j - 1];
+            }
+            
+            if (needle[j] == needle[i]) {
+                j++;
+            }
+            
+            lps[i] = j;
+        }
+        
+        int m = haystack.size();
+        int index = -1;
+        int j = 0;
+        for (int i = 0; i < m; i++) {
+            while (j > 0 && needle[j] != haystack[i]) {
+                j = lps[j - 1];
+            }
+            
+            if (needle[j] == haystack[i]) {
+                j++;
+                
+                if (j == n) {
+                    index = i - n + 1;
+                    break;
+                }
+            }
+        }
+        
+        return index;
     }
 };
+
+// TC: O(m * n), m, n = lengths of given two strings
+// SC: O(1)
