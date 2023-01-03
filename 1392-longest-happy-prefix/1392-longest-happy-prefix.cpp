e0f81d4e186266992be0025bd3d06f731264dfdc
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-03 13:40:26 +0530

    Time: 51 ms (64.01%), Space: 20.1 MB (40.66%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     string longestPrefix(string s) {
         int n = s.size();
-
         vector<int> lps(n);
-        for (int i = 1; i < s.size(); i++) {
-            if (s[i] == s[lps[i - 1]]) {
-                lps[i] = lps[i - 1] + 1;
-            } else {
-                int j = lps[i - 1];
-
-                while (j > 0 && s[i] != s[j]) {
-                    j = lps[j - 1];
-                }
-
-                if (s[i] == s[j])
-                    j++;
-
-                lps[i] = j;
+        
+        for (int i = 1; i < n; i++) {
+            int j = lps[i - 1];
+            
+            while (j > 0 && s[j] != s[i]) {
+                j = lps[j - 1];
             }
+            
+            if (s[j] == s[i])
+                j++;
+            
+            lps[i] = j;
         }
-
+        
         return s.substr(0, lps[n - 1]);
     }
 };
+
+// TC: O(n)
+// SC: O(n)
