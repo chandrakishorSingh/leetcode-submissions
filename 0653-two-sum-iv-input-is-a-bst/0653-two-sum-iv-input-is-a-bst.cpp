d104feb81ee9a7680f9ae997984cde67db8c4410
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-10 17:28:06 +0530

    Time: 119 ms (7.87%), Space: 36.9 MB (68.79%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        TreeNode* current = root;
+        
+        stack<TreeNode*> st1;
+        while (current != nullptr) {
+            st1.push(current);
+            current = current->left;
+        }
+        
+        current = root;
+        stack<TreeNode*> st2;
+        while (current != nullptr) {
+            st2.push(current);
+            current = current->right;
+        }
+        
+        auto left = st1.top();  st1.pop();
+        auto right = st2.top(); st2.pop();
+        while (left != right) {
+            int sum = left->val + right->val;
+            
+            cout << left->val << " " << right->val << endl;
+            
+            if (sum == k)
+                return true;
+            
+            if (sum < k) {
+                // left = st1.top();
+                // st1.pop();
+                
+                current = left->right;
+                while (current != nullptr) {
+                    st1.push(current);
+                    current = current->left;
+                }
+                
+                left = st1.top();
+                st1.pop();
+            } else {
+                // right = st2.top();
+                // st2.pop();
+                
+                current = right->left;
+                while (current != nullptr) {
+                    st2.push(current);
+                    current = current->right;
+                }
+                
+                right = st2.top();
+                st2.pop();
+            }
+        }
+        
+        return false;
+    }
+};
