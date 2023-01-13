828fddbc303d91a568eb230d330651714f882d90
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-13 12:11:42 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.88%) - LeetHub

---
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+        string result = "";
+        
+        bool isBulkyObject = isBulky(length, width, height, mass);
+        bool isHeavyObject = isHeavy(mass);
+        
+        vector<vector<string>> categories = {{"Neither", "Heavy"}, {"Bulky", "Both"}};
+        
+        return categories[isBulkyObject][isHeavyObject];
+    }
+    
+    bool isBulky(int length, int width, int height, int mass) {
+        int64_t volume = (int64_t)length * width * height;
+        
+        return (volume >= 1e9) || (length >= 1e4) || (width >= 1e4) || (height >= 1e4);
+    }
+    
+    bool isHeavy(int mass) {
+        return mass >= 100;
+    }
+};
+
+// TC: O(1)
+// SC: O(1)
