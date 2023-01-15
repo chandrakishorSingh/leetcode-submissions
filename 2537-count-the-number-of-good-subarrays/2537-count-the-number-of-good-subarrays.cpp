f8f2fd353db2664a96329f213c9a658100f02923
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-15 13:08:56 +0530

    Time: 219 ms (75.00%), Space: 77.3 MB (12.50%) - LeetHub

---
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> matches(n);
+        unordered_map<int, int> freq;
+        
+        long long result = 0;
+        int start = 0;
+        int matchCount = 0;
+        for (int end = 0; end < n; end++) {
+            freq[nums[end]]++;
+            
+            if (freq[nums[end]] > 1) {
+                matchCount += freq[nums[end]] - 1;
+            }
+            
+            while (matchCount >= k) {
+                result += n - end;
+
+                matchCount -= freq[nums[start]] - 1;
+                freq[nums[start]]--;
+                start++;
+            }
+        }
+        
+        return result;
+    }
+};
+
+// TC: O(n)
+// SC: O(n)
