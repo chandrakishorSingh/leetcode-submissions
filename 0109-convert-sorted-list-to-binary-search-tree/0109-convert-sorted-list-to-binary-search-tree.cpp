85518817deea427fc64b093a90eaff45bb569150
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-25 12:29:34 +0530

    Time: 34 ms (52.35%), Space: 28.6 MB (34.90%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> nums;
+        
+        ListNode* current = head;
+        while (current != nullptr) {
+            nums.push_back(current->val);
+            current = current->next;
+        }
+        
+        return dfs(nums, 0, nums.size() - 1);
+    }
+    
+    TreeNode* dfs(vector<int>& nums, int left, int right) {
+        if (left > right)
+            return nullptr;
+        
+        int mid = left + (right - left) / 2;
+        
+        auto node = new TreeNode(nums[mid]);
+        node->left = dfs(nums, left, mid - 1);
+        node->right = dfs(nums, mid + 1, right);
+        
+        return node;
+    }
+};
