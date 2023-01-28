d960a16b9db255660530d9842b717b070dd8cb1b
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-01-28 12:56:31 +0530

    Time: 19 ms (92.10%), Space: 13.9 MB (59.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegrees(numCourses);
-        
         vector<vector<int>> adjList(numCourses);
         for (auto& prerequisite: prerequisites) {
             int u = prerequisite[0];
             int v = prerequisite[1];
             
             adjList[v].push_back(u);
-            indegrees[u]++;
         }
         
-        queue<int> que;
+        vector<int> processed(numCourses);
+        vector<int> visited(numCourses);
         for (int i = 0; i < numCourses; i++) {
-            if (indegrees[i] == 0) {
-                que.push(i);
+            if (!processed[i] && isCyclic(i, adjList, visited, processed)) {
+                return false;
             }
         }
         
-        while (numCourses != 0 && !que.empty()) {
-            auto node = que.front();
-            que.pop();
-            
-            for (auto neighbour: adjList[node]) {
-                indegrees[neighbour]--;
-                
-                if (indegrees[neighbour] == 0) {
-                    que.push(neighbour);
-                }
+        return true;
+    }
+    
+    bool isCyclic(int node, vector<vector<int>>& adjList, vector<int>& visited, vector<int>& processed) {
+        visited[node] = 1;
+        
+        bool result = false;
+        for (auto neighbour: adjList[node]) {
+            if (visited[neighbour]) {
+                result = true;
+                break;
             }
             
-            numCourses--;
+            if (!processed[node]) {
+               result |= isCyclic(neighbour, adjList, visited, processed); 
+            }
+            
+            if (result) {
+                break;
+            }
         }
         
-        return numCourses == 0;
+        processed[node] = 1;
+        visited[node] = 0;
+        
+        return result;
     }
 };
 
-// TC: O(n * log(n + e))
+// TC: O(n + e)
 // SC: O(n + e)
