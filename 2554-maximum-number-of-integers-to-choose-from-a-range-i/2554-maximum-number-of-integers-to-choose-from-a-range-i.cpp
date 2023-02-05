bceabc5c1299d61b0bbd53cc593607872fd51dd0
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-02-05 07:27:36 +0530

    Time: 215 ms (100.00%), Space: 117.7 MB (44.44%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
-        unordered_set<int> st;
+        vector<int> nums(n + 1);
         for (auto num: banned) {
-            st.insert(num);
+            if (num <= n) {
+                nums[num] = 1;
+            }
         }
         
         int result = 0;
         int sum = 0;
         for (int i = 1; i <= n; i++) {
-            if (st.count(i)) {
+            if (nums[i]) {
                 continue;
             }
             
