fc693e4109698e159903662a7fef09105475a78c
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-08-18 13:01:41 +0530

    Time: 349 ms (82.69%), Space: 118.7 MB (49.35%) - LeetHub

---
+++ 2564-substring-xor-queries/2564-substring-xor-queries.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> substringXorQueries(string s, vector<vector<int>>& queries) {
+        map<int, vector<int>> substrings;
+        
+        for (int i = 0; i < s.size(); i++) {
+            int value = 0;
+            int limit = min((int)s.size() - 1, i + 30);
+
+            for (int j = i; j <= limit; j++) {
+                value = (value << 1) + (s[j] - '0');
+                
+                auto it = substrings.find(value);
+                if (it == substrings.end()) {
+                    substrings[value] = {i, j};
+                } else {
+                    int oldRange = it->second[1] - it->second[0];
+                    int newRange = j - i;
+                    
+                    if (newRange < oldRange) {
+                        it->second = {i, j};
+                    }
+                }
+            }
+        }
+        
+        vector<vector<int>> result;
+        for (auto& query: queries) {
+            auto it = substrings.find(query[0] ^ query[1]);
+            
+            if (it == substrings.end()) {
+                result.push_back({-1, -1});
+            } else {
+                result.push_back(it->second);
+            }
+        }
+        
+        return result;
+    }
+};
