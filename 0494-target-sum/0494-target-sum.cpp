7d6664785a18a96e8366a4b9c8999ca857c360ef
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-09-19 17:01:42 +0530

    Time: 7 ms (91.39%), Space: 9.8 MB (59.61%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,4 @@
 class Solution {
-private:
-    int countSubsetSum(vector<int>& nums, int target, int n) {
-        if (n == 0) {
-            return target == 0 ? 1 : 0;
-        }
-        
-        int option1 = 0;
-        if (nums[n - 1] <= target) {
-            option1 = countSubsetSum(nums, target - nums[n - 1], n - 1);
-        }
-        
-        int option2 = countSubsetSum(nums, target, n - 1);
-        
-        return option1 + option2;
-    }
-    
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int totalSum = accumulate(nums.begin(), nums.end(), 0);
@@ -26,24 +10,24 @@ public:
         
         int n = nums.size();
         int width = (totalSum + target) / 2;
-        vector<vector<int>> store(n + 1, vector<int>(width + 1));
+        vector<int> store(width + 1);
         
-        store[0][0] = 1;
+        store[0] = 1;
         
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= width; j++) {
+            for (int j = width; j >= 0; j--) {
                 int option1 = 0;
                 if (nums[i - 1] <= j) {
-                    option1 = store[i - 1][j - nums[i - 1]];
+                    option1 = store[j - nums[i - 1]];
                 }
                 
-                int option2 = store[i - 1][j];
+                int option2 = store[j];
                 
-                store[i][j] = option1 + option2;
+                store[j] = option1 + option2;
             }
         }
         
-        return store[n][width];
+        return store[width];
     }
 };
 
