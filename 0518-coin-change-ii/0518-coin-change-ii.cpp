acfe43e33c8f89a7c026959198e3384595ad67e1
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-10-03 18:46:11 +0530

    Time: 16 ms (56.16%), Space: 7.4 MB (73.28%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -5,19 +5,17 @@ public:
         vector<int> current(amount + 1);
         current[0] = 1;
         
-        vector<int> next = current;
+        // vector<int> next = current;
         
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= amount; j++) {
                 if (j >= coins[i - 1]) {
-                    next[j] = current[j] + next[j - coins[i - 1]];
+                    current[j] = current[j] + current[j - coins[i - 1]];
                     // cout << "Adding " << current[j] << " and " << next[j - coins[i - 1]] << endl;
-                } else {
-                    next[j] = current[j];
                 }
             }
             
-            current = next;
+            // current = next;
             // cout << i << ' ';
             // for (auto it: current) {
             //     cout << it << ' ';
