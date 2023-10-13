26c7e64fabaa877f014d6d556a65d0cf3b033db9
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2023-10-13 19:39:48 +0530

    Time: 50 ms (57.43%), Space: 7.1 MB (88.33%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,36 +1,28 @@
 class Solution {
 private:
-    int _longestCommonSubsequence(string& text1, string& text2, int n1, int n2, vector<vector<int>>& store) {
-        if (n1 == 0 || n2 == 0) {
-            return 0;
+    int _lcs(string& text1, string& text2) {
+        if (text1.size() < text2.size()) {
+            return _lcs(text2, text1);
         }
-
-        if (store[n1][n2] != -1) {
-            return store[n1][n2];
-        }
-
-        int result = -1;
-        if (text1[n1 - 1] == text2[n2 - 1]) {
-            result = 1 + _longestCommonSubsequence(text1, text2, n1 - 1, n2 - 1, store);
-        } else {
-            result = max(
-                _longestCommonSubsequence(text1, text2, n1 - 1, n2, store),
-                _longestCommonSubsequence(text1, text2, n1, n2 - 1, store)
-            );
-        }
-
-
-        store[n1][n2] = result;
-
-        return store[n1][n2];
-    }
-
-    int _lcs(string& s, string& r) {
-        int n1 = s.size();
-        int n2 = r.size();
-        vector<vector<int>> store(n1 + 1, vector<int>(n2 + 1, -1));
         
-        return _longestCommonSubsequence(s, r, n1, n2, store);
+        int n1 = text1.size();
+        int n2 = text2.size();
+        
+        vector<int> store(n2 + 1);
+        
+        for (int i = 1; i <= n1; i++) {
+            int previousState = store[0];
+            
+            for (int j = 1; j <= n2; j++) {
+                int currentState = text1[i - 1] == text2[j - 1] ? 1 + previousState : max(store[j], store[j - 1]);
+                
+                previousState = store[j];
+                store[j] = currentState;
+            }
+        }
+        
+        return store[n2];
+
     }
     
 public:
