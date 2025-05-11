b3ea8d98012e8a485d95a8dea8a39957f60e64c3
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2025-05-11 09:18:27 +0530

    Time: 0 ms, Memory: 13.5 MB - LeetHub

---
+++ 16733-450-74-search-a-2d-matrix/16733-450-74-search-a-2d-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    vector<int> getCoordinatesFromIndex(int index, int numberOfColumns) {
+        int row = index / numberOfColumns;
+        int col = index % numberOfColumns;
+        
+        return {row, col};
+    }
+    
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int low = 0;
+        int high = (n * m) - 1;
+
+        bool result = false;
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            
+            vector<int> cell = getCoordinatesFromIndex(mid, m);
+            int row = cell[0];
+            int col = cell[1];
+            
+            if (matrix[row][col] > target) {
+                high = mid - 1;
+            } else if (matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                result = true;
+                break;
+            }
+        }
+        
+        return result;
+    }
+};
+
+// TC: O(log(m * n))
+// SC: O(1)
