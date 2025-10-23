3fa74816f3cb5ec4b1c010d663355080ca275850
Chandrakishor Singh <42005550+chandrakishorSingh@users.noreply.github.com>
2025-10-23 20:12:45 +0530

    Time: 24 ms, Memory: 43.5 MB - LeetHub

--- 0740-59-1115-print-foobar-alternately/0740-59-1115-print-foobar-alternately.java
+++ 0740-59-1115-print-foobar-alternately/0740-59-1115-print-foobar-alternately.java
@@ -1,6 +1,8 @@
 class FooBar {
     private int n;
-    private volatile int turn = 0;
+    private int turn = 0;
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition turnChanged = lock.newCondition();
 
     public FooBar(int n) {
         this.n = n;
@@ -9,22 +11,41 @@ class FooBar {
     public void foo(Runnable printFoo) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            while (turn != 0) {}
-            
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
-        	printFoo.run();
-            turn = 1;
+            try {
+                lock.lock();
+                
+                while (turn != 0) {
+                    turnChanged.await();
+                }
+                
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                turn = 1;
+                turnChanged.signal();
+            } finally {
+                lock.unlock();
+            }
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            while (turn != 1) {}
+            try {
+                lock.lock();
+                
+                while (turn != 1) {
+                    turnChanged.await();
+                }
+                
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                turn = 0;
+                turnChanged.signal();
+            } finally {
+                lock.unlock();
+            }
             
-            // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
-            turn = 0;
         }
     }
 }
